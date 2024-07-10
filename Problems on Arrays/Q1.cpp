e0da3f06bc246137e952e02463b2de63628cdbf9
soura07 <nitsouradeep2022@gmail.com>
2024-07-10 11:15:54 +0530

    new commit

--- Problems on Arrays/Q1.cpp
+++ Problems on Arrays/Q1.cpp
@@ -1,6 +1,22 @@
-#include<bits/stdc++.h>
+// Find the smallest number in an array
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
+int sortArr(vector<int> arr){
+    sort(arr.begin(), arr.end());
+    return arr[0];
+
+
+}
 int main()
 {
-    cout << "Hello";
+    vector<int> arr1 = {2, 5, 1 , 3 , 0};
+    vector<int> arr2 = {8, 10, 5, 7, 9};
+
+    cout<<"Smallest Nummber is " << sortArr(arr1) << endl;
+    cout<<"Smallest Nummber is " << sortArr(arr2) << endl;
+
+    return 0;
 }
